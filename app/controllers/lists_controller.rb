441ed748a9966342084f2f3cb5fe2303d4570cd6
Caroline Gognet <carolinegognet@gmail.com>
2021-11-11 16:13:29 +0100

    good progress

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -12,6 +12,21 @@ class ListsController < ApplicationController
   end
 
   def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
 
+  def destroy
+
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name, :image_url)
   end
 end
